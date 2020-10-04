2f12ef8e137d699a627eb74374490341d74ba23e
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-10-04 11:57:31 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,50 @@
+//Zadanie 1
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1 == bool2);
+
+//pojedynczy "=" porownuje wartosci, podwojny "==" porownuje wartosci i typy
+
+//Zadanie 2
+let num1=1;
+let num2=2;
+
+let moduloResult=0;
+
+moduloResult=num1%num2;
+
+console.log(moduloResult);
+
+//Zadanie 3
+let stringOne = "abc";
+let stringTwo = "xyz";
+let stringResult = "";
+stringResult = stringOne+stringTwo;
+console.log(stringResult);
+
+//Zadanie 4
+let someNumber = 425;
+let someString = "425";
+
+console.log(someNumber==someString); //porownuje wartosci
+console.log(someNumber===someString); //poruwnuje wartosci i typy
+
+//Zadanie 5
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//Zadanie 6
+let num1 = 1;
+let num2 = 2;
+let result = null;
+result = num1 > num2;
+console.log(result);
+
+
+
+
+
