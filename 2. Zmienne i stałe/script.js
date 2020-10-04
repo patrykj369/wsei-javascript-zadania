77a705ec323a9461943c4970be7b52160dbf4838
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-10-04 13:01:03 +0200

    Update script.js

--- 2. Zmienne i stałe/script.js
+++ 2. Zmienne i stałe/script.js
@@ -1,2 +1,23 @@
+//Zadanie 1
+let number = 1;
+let string = 'xyz';
+let stringWithNumber = "dwa" + 2;
+let logic = 2 < 4;
+let special = null;
+
+console.log(number);
+console.log(string);
+console.log(stringWithNumber);
+console.log(logic);
+console.log(special);
+
+//Zadanie 2
+let numberOne = 2;
+let numberTwo = 3;
+let sum = 0;
+sum = numberOne + numberTwo;
+console.log(sum);
+
+//Zadanie 3
 let test;
 console.log(test); //jest undefined bo nie ma wartości
