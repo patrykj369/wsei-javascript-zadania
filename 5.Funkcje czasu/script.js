0ccc3cd03f8260386d888463ecec6579c2c961ba
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-10-18 11:35:49 +0200

    Update script.js

---
+++ 5.Funkcje czasu/script.js
@@ -0,0 +1,27 @@
+//Zadanie 1
+let time = 0;
+const interval = setInterval(() => {
+    time += 1;
+    console.log(`Cześć po raz ${time}`);
+    if(time >= 15) {
+        clearInterval(interval);
+    }
+}, 1000);
+
+//Zadanie2
+let myArray = [1, 2, 3, 4];
+let myTimer = setTimeout(() => {
+    for (let i=0; i < myArray.length; i++) {
+        console.log (myArray[i]);
+    }
+    let j = 0
+    let myInterval = setInterval(() => {
+        console.log (myArray[j]);
+        j++
+        if (j >= myArray.length) {
+            clearInterval(myInterval);
+        }
+    }, 3000)
+    
+}, 2000 )
+    
