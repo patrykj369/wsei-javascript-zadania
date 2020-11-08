3085202171cbcfbf60960e0a24585baed7241a58
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-11-08 11:58:43 +0100

    Update script.js

--- 8.Obiekty/script.js
+++ 8.Obiekty/script.js
@@ -35,4 +35,42 @@ newObject.sumValues([1, 5, 7]);
 console.log(newObject.sum);
 
 //4
+var car={
+    name:"BMW",
+    age: 12
+}
+
+
+for(let key in car){
+    console.log(`${key}: ${car[key]}`);
+}
+
+//5
+const car = {
+	name: 'Fiat',
+	age: 35,
+	color: 'red'
+};
+const carCountryCity = {
+	country: 'Poland',
+	city: 'Krakow'
+};
+car.production = carCountryCity;
+console.log(car.production.country, car.production.city);
+
+//6
+
+const car = {
+	name: 'Fiat',
+	age: 35,
+	color: 'red'
+};
+
+car.width = 156;
+
+car.hello = () => {
+    console.log("Hello");
+}
+
+car.hello();
 
