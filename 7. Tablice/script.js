e8c150cb8bb385f2f1f1f267c0a343f7fa0dc9a1
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-11-08 11:39:15 +0100

    Update script.js

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -86,3 +86,34 @@ function myTable(paramTable){
     }
 };
 myTable([1,2,3,4]);
+
+//8
+myTable([1,2,3,4]);
+
+function myTable(paramTable){
+    var sum =0;
+    var length = 0;
+    for (i=0; i<paramTable.length; i++){
+        if(paramTable[i] % 2 == 0){
+            
+            sum = sum + paramTable[i];
+            length++;
+        }
+            
+    }
+    let avg = sum / length;
+    console.log(avg)
+};
+
+//9
+function reserveSing(paramTable)
+{
+  return  paramTable.map(el => {
+    return el * (-1);
+  })
+}
+console.log(reserveSing([1,2,3,4,6,-6]));
+
+//10
+
+
