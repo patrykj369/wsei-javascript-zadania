033d548317a73e7f9b862822811e2b8e4be3aaa5
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-12-06 11:46:38 +0100

    Update script.js

--- 12.DOM-poruszanie/script.js
+++ 12.DOM-poruszanie/script.js
@@ -1,5 +1,4 @@
 //Zad 1
-
 const buz = document.querySelector("#buz");
 const baz = document.querySelector("#baz");
 const foo = document.querySelector("#foo");
@@ -10,3 +9,12 @@ console.log(foo.children);
 console.log(foo.parentNode);
 console.log(foo.firstElementChild);
 console.log(foo.children[Math.floor(foo.children.length / 2)]);
+
+// zad 2
+const displayText = (element) => {
+    element.addEventListener("click", () => {
+      const elementWithText = element.querySelector("div div div div div:nth-child(2) div div"); 
+      const textToDisplay = elementWithText.innerHTML
+      console.log(textToDisplay);
+    });
+};
