c55485b256bba7505f147b60797d765a77263ba5
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-12-06 11:51:16 +0100

    Update script.js

--- 9.Konstruktory i prototype/script.js
+++ 9.Konstruktory i prototype/script.js
@@ -47,3 +47,31 @@ calc1.sub(2,3)
 calc1.mul(3,3)
 calc1.div(12,3)
 calc1.showMemory()
+
+//Zad 3
+function Game(){
+    
+   this.lastNumber = 0;
+   this.number = 0;
+   this.generator = null;
+    
+    this.startGenerator = () => {
+        this.generator = setInterval(() => {
+            this.lastNumber = this.number;
+            this.number = Math.floor((Math.random() * 10) + 1);
+            
+            console.log(this.number);
+            this.checkWin();
+        }, 1000)
+    }
+    
+    this.checkWin = () => {
+        if(this.lastNumber + 5 === this.number){
+            console.log("Wygrałeś");
+            clearInterval(this.generator);
+        }
+    }
+}
+
+let game1 = new Game();
+game1.startGenerator();
