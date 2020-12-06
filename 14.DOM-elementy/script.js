f13121da9ebf44938d20fb6b8ae6996305160e75
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-12-06 17:09:05 +0100

    Update script.js

--- 14.DOM-elementy/script.js
+++ 14.DOM-elementy/script.js
@@ -7,3 +7,21 @@ function getTags(elements) {
 }
 
 console.log(getTags(divs));
+
+//Zadanie 2
+const shortList = document.querySelector(".short-list");
+
+function displayDetails(item) {
+//2.1    
+    console.log(item.innerHTML);
+//2.2    
+    console.log(item.outerHTML);
+//2.3    
+    console.log(item.className);
+//2.4    
+    console.log(item.classList);
+//2.5    
+    console.log(item.dataset);
+}
+
+displayDetails(shortList);
