4f49cafb409e77facf74bffb0570a34c4ae9ac9b
patrykj369 <63154012+patrykj369@users.noreply.github.com>
2020-12-06 20:20:27 +0100

    Update script.js

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -32,3 +32,31 @@ document.body.appendChild(button);
 button.addEventListener("click", e => {
     e.target.remove();
 })
+
+//Zadanie 5
+const randomNumber = Math.floor(Math.random() * 15);
+for (let i = 0; i < randomNumber; i++) {
+    const randomDiv = document.createElement("div");
+    randomDiv.innerText = "to jest div numer: " + i;
+    document.body.appendChild(randomDiv);
+    
+}
+
+//Zadanie 6
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const div3 = document.createElement("div");
+const span1 = document.createElement("span");
+const span2 = document.createElement("span");
+
+div1.textContent = "to jest div";
+div3.textContent = "to jest div";
+span1.textContent = " to jest span";
+span2.textContent = "to jest span";
+
+div2.appendChild(div3);
+
+document.body.appendChild(div1);
+document.body.appendChild(span1);
+document.body.appendChild(div2);
+document.body.appendChild(span2);
